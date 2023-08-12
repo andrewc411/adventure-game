bec9bfa154aa952cd8f80a67c4e711f1483871e8
Andrew Carroll <andrewc411@yahoo.com>
2023-08-11 21:48:09 -0400

    complete js functionality

--- main.js
+++ main.js
@@ -27,11 +27,16 @@ function showTextNode(textNodeIndex) {
 }
 
 function showOption(option) {
-    return true
+    return option.requiredState == null || option.requiredState(state)
 }
 
 function selectOption(option) {
-
+    const nextTextNodeId = option.nextText
+    if (nextTextNodeId <= 0) {
+        return startGame()
+    }
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeId)
 }
 
 const textNodes = [
@@ -51,7 +56,54 @@ const textNodes = [
         ]
     },
     {
-        id: 2
+        id: 2,
+        text: "You venture forth in search of answers to where you are when you come across a merchant.",
+        options: [
+            {
+                text: "You trade the goo for a sword.",
+                requiredState: (currentState) => currentState.blueGoo,
+                setState: { blueGoo: false, sword: true},
+                nextText: 3
+            },
+            {
+                text: "You trade the goo for a shield.",
+                requiredState: (currentState) => currentState.blueGoo,
+                setState: { blueGoo: false, shield: true},
+                nextText: 3
+            },
+            {
+                text: "Ignore the merchant.",
+                nextText: 3
+            },
+        ]
+    },
+    {
+        id: 3,
+        text: "After leaving the merchant you start to feel tired and stumble upon a small town next to a dangerous looking castle.",
+        options: [
+            {
+                text: "Explore the castle",
+                nextText: 4
+            },
+            {
+                text: "Find a room to sleep at in the town.",
+                nextText: 5
+            },
+            {
+                text: "Find some hay in a stable to sleep in.",
+                nextText: 6
+            }
+        ]
+    },
+    {
+        id: 4,
+        text: "You are so tired that you fall asleep while exploring the castle and you are killed when a terrible monster finds you.",
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
     }
 ]
 
