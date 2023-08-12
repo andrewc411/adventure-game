46cc7447c53fe98e94d38231d936e0bab67a8a92
Andrew Carroll <andrewc411@yahoo.com>
2023-08-11 22:55:41 -0400

    adding to the story

--- main.js
+++ main.js
@@ -104,7 +104,103 @@ const textNodes = [
                 nextText: -1
             }
         ]
-    }
+    },
+    {
+        id: 5,
+        text: "You don't have money for a room and are thrown in jail.",
+        options: [
+            {
+                text: 'Restart',
+                nextText: -1
+            }
+        ]
+    },
+    {
+        id: 6,
+        text: "You find a stable to sleep in and are well rested when a horse eating the hay wakes you after mistaking your hair for its breakfast",
+        options: [
+            {
+                text: "You are quick to anger and punch the horse in the nose.",
+                nextText: 7
+            },
+            {
+                text: "You leave the stable and head towards the castle.",
+                nextText: 8
+            },
+            {
+                text: "Steal the horse.",
+                setState: { horse: true },
+                nextText: 9
+
+            }
+        ]
+    },
+    {
+        id: 7,
+        text: "The horse squeals and alerts a young farmer's boy who comes at you with a pitchfork.",
+        options: [
+            {
+                text: "You use your shield to protect yourself.",
+                requiredState: (currentState) => currentState.shield,
+                nextText: 10
+            },
+            {
+                text: "You unsheathe your sword and stab the bastard.",
+                requiredState: (currentState) => currentState.sword,
+                nextText: 11
+
+            },
+            {
+                text: "Flash your sword and tell the boy you don't want trouble.",
+                requiredState: (currentState) => currentState.sword,
+                nextText: 10
+
+            },
+            {
+                text: "Run away.",
+                nextText: 12
+            }
+        ]
+    },
+    {
+        id: 8,
+        text: "You are almost at the castle when you are met by a skilled warrior.",
+        options: [
+            {
+                text: "Challenge the warrior to a duel.",
+                requiredState: (currentState) => currentState.sword || currentState.shield,
+                nextText: 13
+            },
+            {
+                text: "He acknowledges you and offers to train you. You accept...",
+                requiredState: (currentState) => currentState.shield || currentState.sword,
+                nextText: 14
+            },
+            {
+                text: "He acknowledges you and offers to train you. You decline...",
+                requiredState: (currentState) => currentState.shield || currentState.sword,
+                nextText: 15
+            },
+            {
+                text: "Avoid the warrior. Try not to disturb him.",
+                nextText: 15
+            }
+        ]
+    },
+    {
+        id: 9,
+        text: "You take off into the rising sun with your new friend. This will be the greatest adventure! With the wind rushing through your hair your horse gets thirsty as you come upon a river with a bridge to cross.",
+        options: [
+            {
+                text: "Stop at the river for your horse to drink.",
+                nextText: 16
+            },
+            {
+                text: "Cross the bridge without stopping.",
+                nextText: 17
+            }
+        ]
+    },
 ]
 
 startGame();
